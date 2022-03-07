2dbf60af53514afd7a1a5ed181d397deee289683
PrakshalTricon <prakshal.jain@triconinfotech.com>
2022-03-07 17:11:52 +0530

    transaction hit

--- src/main/java/com/readData/DataXML/commons/DataMapping.java
+++ src/main/java/com/readData/DataXML/commons/DataMapping.java
@@ -10,13 +10,15 @@ public class DataMapping {
     public static final String GROUPMASTER = "GroupMaster";
     public static final String VOUCHERTRANSACTION = "Transaction";
     public static final String COSTCENTER = "CostCenter";
+    public static final String VOUCHERTRANSACTIONALTERGUID = "TransactionAlterGuid";
 
     public static final Map<String,String> typeXMLMapper = Map.ofEntries(
             Map.entry(LEDGER,"Req_LedgerMaster.xml"),
             Map.entry(VOUCHER,"Req_VoucherMaster.xml"),
             Map.entry(GROUPMASTER,"Req_GroupMaster.xml"),
             Map.entry(VOUCHERTRANSACTION,"Req_Transaction.xml"),
-            Map.entry(COSTCENTER,"Req_CostCenterMaster.xml")
+            Map.entry(COSTCENTER,"Req_CostCenterMaster.xml"),
+            Map.entry(VOUCHERTRANSACTIONALTERGUID,"Req_Transaction_Alter_Guid.xml")
 
     );
 }
